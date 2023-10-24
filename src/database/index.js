0cf707ea71e2d3cf7a1b5060face10b601bc199b
Sparsh Shandilya <sparshshandilya123@gmail.com>
2023-10-24 23:51:28 +0530

    Initial commit

---
+++ src/database/index.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const configOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+const connectToDB = async () => {
+  const connectionUrl =
+    "mongodb+srv://sparshshandilya123:sparsh123@cluster0.dk2h0gw.mongodb.net/";
+
+  mongoose
+    .connect(connectionUrl, configOptions)
+    .then(() => console.log("Ecommerce database connected successfully!"))
+    .catch((err) =>
+      console.log(`Getting Error from DB connection ${err.message}`)
+    );
+};
+
+export default connectToDB;
